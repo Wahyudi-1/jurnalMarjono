5b2620bd14eff809c517fdbbca07d421288318c3
Wahyudi-1 <147951968+Wahyudi-1@users.noreply.github.com>
2025-07-16 19:19:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
 // TAHAP 1: KONFIGURASI GLOBAL DAN STATE APLIKASI
 // ====================================================================
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycby-tvNoDtkx3jID_oqWDItDVWRfQwhVPl0ByWqdD3LX6z2Rp8FuvcexZ1NrWdLMI6dLMw/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzFm1zAQ9Q1itCgxyWxNelDGxqbRUymqYl_1KEkX-6dEX0R0nd09EuhQ6ur5olCmodk/exec";
 
 let cachedSiswaData = [];
 let cachedJurnalHistory = [];
